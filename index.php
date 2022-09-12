427a93eb4ec390db2b918b36e7e1083bc84eb43a
ardus-uk <peter@ardus.co.uk>
2022-09-12 17:22:45 +0100

    Tidy index.php to display IP addresses

--- index.php
+++ index.php
@@ -5,35 +5,28 @@
 <body style="font-family: Sans-Serif;">
 <?php 
 
-echo "<h2>Skiddaw U3A Computers and Control Group</h2>\n";
+echo "<p>rpi-utils | ".$_SERVER['SERVER_NAME']." | ".$_SERVER['SERVER_ADDR']."</p>\n";
 echo "<h3>Raspberry Pi IP addresses</h3>\n"; 
 
 $column_colours = ["lightgrey","lightcyan","peachpuff","lightblue","lightgreen","khaki"];
+$column_heads = ["Machine name","Wired","Wireless","Date","Time","Router IP address"];
+$number_of_cols = count($column_colours);
+$col_width = floor(100/$number_of_cols);
 echo '<table style="text-align:center; border="0">';
 echo "\n";
 echo '<tr>';
-echo "\n";
-echo '<th width="20%" bgcolor= "'.$column_colours[0].'">Machine name</th>';
-echo "\n";
-echo '<th width="15%" bgcolor= "'.$column_colours[1].'">Wired</th>';
-echo "\n";
-echo '<th width="15%" bgcolor= "'.$column_colours[2].'">Wireless</th>';
-echo "\n";
-echo '<th width="15%" bgcolor= "'.$column_colours[3].'">Date</th>';
-echo "\n";
-echo '<th width="15%" bgcolor= "'.$column_colours[4].'">Time</th>';
-echo "\n";
-echo '<th width="20%" bgcolor= "'.$column_colours[5].'">Router IP address</th>';
+for ($i = 0; $i < $number_of_cols; $i++)
+{
+    echo "\n";
+    echo '<th width="'.$col_width.'%" bgcolor= "'.$column_colours[$i].'">'.$column_heads[$i].'</th>';
+}
 echo "\n";
 echo '</tr>';
 echo "\n";
 
-
 $rpi_files = glob("*.txt");
 # print_r($rpi_files);
 foreach ($rpi_files as  $key => $rpi_file) {
-   # echo  $key ." =>" .$rpi_file;
-#   if ($rpi_file[10]!='p') {
    $fh = fopen($rpi_file, "r") or die("Unable to open file!");
    $fcontents = fread($fh,filesize($rpi_file));
    $fitems = explode("\n",$fcontents);
@@ -55,7 +48,6 @@ foreach ($rpi_files as  $key => $rpi_file) {
                 echo "</td>\n";
                 echo "</tr>\n";
    fclose($fh);
- #  }
 }
 ?>
 </table>
